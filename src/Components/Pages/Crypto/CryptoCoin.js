88bbeb0aec3c63865154dd9af7ab792ca5b7fca3
leonmezu1 <leo.mez@outlook.com>
2020-11-17 00:09:57 -0500

    Trimmed project

--- src/Components/Pages/Crypto/CryptoCoin.js
+++ src/Components/Pages/Crypto/CryptoCoin.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { geckoCoin } from '../../../Config/Axios';
+import { setFetching } from '../../../Actions';
+import Spinner from '../../Spinner/Spinner';
 
-const CryptoCoin = () => <h1>Crypto</h1>;
+const CryptoCoin = () => {
+  const { id } = useParams();
+  const currency = useSelector(state => state.CoinStoreState.currency);
+  const order = useSelector(state => state.CoinStoreState.order);
+  const loading = useSelector(state => state.CoinStoreState.loading);
+  const dispatch = useDispatch();
+
+  const [coin, setCoin] = useState({});
+
+  useEffect(async () => {
+    dispatch(setFetching(true));
+    const data = await geckoCoin(id, currency, order, 1);
+    if (data) {
+      setCoin(data[0]);
+      dispatch(setFetching(false));
+    }
+  }, [currency, order]);
+
+  return (
+    <>
+      <h1>{`Id is: ${id}`}</h1>
+      {!loading && coin ? <h2>{`Coin loaded ${coin.symbol}`}</h2> : <Spinner />}
+    </>
+  );
+};
 
 export default CryptoCoin;
